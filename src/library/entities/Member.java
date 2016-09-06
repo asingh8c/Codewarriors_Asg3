be1fa55bf98a8a2f1eecc024da5b034a3758a57e
Asmita <asmitasingh15@gmail.com>
2016-09-06 22:04:23 +1000

    Attributes declared and methods defined for setting and getting attributes of Member entity

--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -1,63 +1,108 @@
 package library.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import library.interfaces.entities.EMemberState;
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
 
-public class Member implements IMember{
+public class Member implements IMember {
 
-	public Member() {
-		// TODO Auto-generated constructor stub
+	private String firstName;
+	private String lastName;
+	private String contactPhone;
+	private String email;
+	private int id;
+	private EMemberState state;
+	private List<ILoan> loanList;
+	private float totalFine;
+
+	public Member(String fName, String lName, String contactPh, String email, int membId) {
+		// Initialize member attributes
+		this.firstName = fName;
+		this.lastName = lName;
+		this.contactPhone = contactPh;
+		this.email = email;
+		this.id = membId;
+		this.loanList = new ArrayList<ILoan>(); // Empty Loan List
+		this.totalFine = 0;
+		this.state = EMemberState.BORROWING_ALLOWED; // Initial state of new
+														// Member
+
 	}
 
 	@Override
 	public boolean hasOverDueLoans() {
-		// TODO Auto-generated method stub
-		return false;
+		// TODO Returns boolean indicating whether user has overdue on loan book
+		for (ILoan loan : this.loanList) {
+			if (loan.isOverDue()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public boolean hasReachedLoanLimit() {
-		// TODO Auto-generated method stub
+		// TODO Returns boolean indicating if number of books allowed for loan
+		// has reached
+		if (this.loanList.size() >= 5) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean hasFinesPayable() {
-		// TODO Auto-generated method stub
+		// TODO Returns boolean indicating if there is pending fine in members account
+		// account
+		if (this.totalFine > 0.0f) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean hasReachedFineLimit() {
-		// TODO Auto-generated method stub
+		// TODO Returns boolean indicating if pending fine has reached limit
+		if(this.totalFine >= 10.0f) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public float getFineAmount() {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO Return total payable fine in member account
+		return this.totalFine;
 	}
 
 	@Override
 	public void addFine(float fine) {
-		// TODO Auto-generated method stub
+		// TODO Add fine in member to existing fine amount
+		if(fine > 0) {
+			this.totalFine += fine;
+		}
+		else {
+			throw new RuntimeException("Member can not be a negative number");
+		}
 		
 	}
 
 	@Override
 	public void payFine(float payment) {
-		// TODO Auto-generated method stub
-		
+		// TODO Update total fine when Member pays off fine
+		if (payment < 0.0f || payment > this.totalFine) {
+			throw new RuntimeException("Negative Value for fine payment and payment greater than total fine is not allowed");
+		}
+		this.totalFine -= payment;
 	}
 
 	@Override
 	public void addLoan(ILoan loan) {
-		// TODO Auto-generated method stub
-		
+		// TODO Add book loan to the loan list
+
 	}
 
 	@Override
@@ -69,7 +114,7 @@ public class Member implements IMember{
 	@Override
 	public void removeLoan(ILoan loan) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override

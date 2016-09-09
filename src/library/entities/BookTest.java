ac8614068ce34fe987c094128d6cf3752850a70d
Prateek <Prateek@TheSign29-PC>
2016-09-09 13:34:25 +1000

    Test Cases Developed
    All test cases for unit cases have been developed and implemented. Few
    cases in which information is needed from other entities has been left
    as they will be covered under integration testing.

--- src/library/entities/BookTest.java
+++ src/library/entities/BookTest.java
@@ -1,9 +1,9 @@
 package library.entities;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 
 /**
  * @author Prateek
@@ -12,74 +12,84 @@ import org.junit.Test;
 
 public class BookTest {
 
-	Book book=new Book("Prateek Narang","Welcome to JUnit Testing.","IT678",7895012);
+	Book book = new Book("Prateek Narang", "Welcome to JUnit Testing.", "IT678", 7895012);
 
 	@Test
 	public void testBook() {
 		boolean status = book instanceof Book;
-		assertTrue("Book created succesfully",status);
+		assertTrue("Book created succesfully", status);
 	}
 
 	@Test
 	public void testBorrow() {
 		// TODO
-		//Needs information from another entity
-		//Will be done in integration testing
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testGetLoan() {
-		 // TODO
+		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testReturnBook() {
 		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testLose() {
-		 // TODO
+		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testRepair() {
 		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testDispose() {
 		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testGetState() {
-		 // TODO
+		// TODO
+		// Needs information from another entity
+		// Will be done in integration testing
 	}
 
 	@Test
 	public void testGetAuthor() {
-		 // TODO
+		String author = book.getAuthor();
+		assertEquals("Prateek Narang", author);
 	}
 
 	@Test
 	public void testGetTitle() {
-		 // TODO
+		String title = book.getTitle();
+		assertEquals("Welcome to JUnit Testing.", title);
 	}
 
 	@Test
 	public void testGetCallNumber() {
-		// TODO
+		String callNumber = book.getCallNumber();
+		assertEquals("IT678",callNumber);
 	}
 
 	@Test
 	public void testGetID() {
-		 // TODO
+		int id = book.getID();
+		assertEquals(7895012, id);
 	}
-
-	@Test
-	public void testToString() {
-		 // TODO
-	}
-
 }

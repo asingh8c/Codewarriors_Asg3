738a3f9ab087e7115e4edf7690f7328e58e0e5cf
Krishanthi <Krishanthi@192.168.0.4>
2016-09-09 23:05:07 +1000

    Create Junit test cases.Have to implement it

---
+++ src/library/entities/LoanTest.java
@@ -0,0 +1,82 @@
+package library.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+/**
+* @author krishanthi
+*
+*/
+public class LoanTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLoan() {
+		fail("Not yet implemented"); //have to implement
+	}
+
+	@Test
+	public void testCommit() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testComplete() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testIsOverDue() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testCheckOverDue() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testGetBorrower() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testGetBook() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented");//have to implement
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");//have to implement
+	}
+
+}

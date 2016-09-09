d57add9df130fc4d90bbfda14b5f362e6b94b467
Prateek <Prateek@TheSign29-PC>
2016-09-09 12:54:38 +1000

    JUnit Testing
    Framework formed. Implementation left.

---
+++ src/library/entities/BookTest.java
@@ -0,0 +1,80 @@
+package library.entities;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+/**
+ * @author Prateek
+ *
+ */
+
+public class BookTest {
+
+	@Test
+	public void testBook() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testBorrow() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetLoan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testReturnBook() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLose() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRepair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDispose() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAuthor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetCallNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}

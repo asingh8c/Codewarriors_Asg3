f810526ffa69d88721df922056fad94a7e1b7685
11580359 <11580359@AUMELSTD0411-12.students.local>
2016-09-13 14:20:17 +1000

    created MemberHelper

---
+++ src/library/daos/MemberHelper.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package library.daos;
+
+import library.interfaces.daos.IMemberHelper;
+import library.interfaces.entities.IMember;
+
+/**
+ * @author thilini
+ *
+ */
+public class MemberHelper implements IMemberHelper {
+
+	/* (non-Javadoc)
+	 * @see library.interfaces.daos.IMemberHelper#makeMember(java.lang.String, java.lang.String, java.lang.String, java.lang.String, int)
+	 */
+	public IMember makeMember(String firstName, String lastName, String contactPhone, String emailAddress, int id) {
+		
+		 return new Member(firstName, lastName, contactPhone, emailAddress, id);
+	}
+
+}
